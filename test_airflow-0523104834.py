bc29e7fdca259cc027f1d45fead1620d7ef485b1
Victor Fonseca <victortales@gmail.com>
2025-05-23 12:48:35 +0200

    Pushed DAG test_airflow-0523104834

---
+++ test_airflow-0523104834.py
@@ -0,0 +1,30 @@
+from airflow.operators.dagrun_operator import TriggerDagRunOperator
+from airflow import DAG
+from airflow.utils.dates import days_ago
+
+args = {
+    "project_id": "test_airflow-0523104834",
+}
+
+dag = DAG(
+    "test_airflow-0523104834",
+    default_args=args,
+    schedule_interval="@once",
+    start_date=days_ago(1),
+    description="""
+Created with Elyra 3.15.0 pipeline editor using `test_airflow.pipeline`.
+    """,
+    is_paused_upon_creation=False,
+)
+
+
+# Operator source: {"catalog_type": "airflow-package-catalog", "component_ref": {"airflow_package": "apache_airflow-1.10.15-py2.py3-none-any.whl", "file": "airflow/operators/dagrun_operator.py"}}
+
+op_869240a6_9133_473e_86a6_02d122d397de = TriggerDagRunOperator(
+    task_id="TriggerDagRunOperator",
+    trigger_dag_id="rom-cylinder-0522041636",
+    python_callable="",
+    execution_date="",
+    executor_config={},
+    dag=dag,
+)
