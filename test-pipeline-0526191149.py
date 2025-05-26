821a612621bf1a403ea752269d6f91d60b0fb89c
Victor Fonseca <victortales@gmail.com>
2025-05-26 19:11:50 +0200

    Pushed DAG test-pipeline-0526191149

---
+++ test-pipeline-0526191149.py
@@ -0,0 +1,91 @@
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+from airflow.contrib.kubernetes.volume_mount import VolumeMount
+from airflow.contrib.kubernetes.volume import Volume
+from airflow.kubernetes.secret import Secret
+from airflow import DAG
+from airflow.utils.dates import days_ago
+
+args = {
+    "project_id": "test-pipeline-0526191149",
+}
+
+dag = DAG(
+    "test-pipeline-0526191149",
+    default_args=args,
+    schedule_interval="@once",
+    start_date=days_ago(1),
+    description="""
+Test pipeline for Custom Airflow Processor with two dependent Python nodes
+    """,
+    is_paused_upon_creation=False,
+)
+
+
+# Operator source: data_processing.py
+
+op_data_processing_node = KubernetesPodOperator(
+    name="Data_Processing",
+    namespace="airflow-elyra",
+    image="continuumio/anaconda3:2023.07-2",
+    cmds=["sh", "-c"],
+    arguments=[
+        "mkdir -p ./jupyter-work-dir/ && cd ./jupyter-work-dir/ && echo 'Downloading https://raw.githubusercontent.com/elyra-ai/elyra/v3.15.0/elyra/airflow/bootstrapper.py' && curl --fail -H 'Cache-Control: no-cache' -L https://raw.githubusercontent.com/elyra-ai/elyra/v3.15.0/elyra/airflow/bootstrapper.py --output bootstrapper.py && echo 'Downloading https://raw.githubusercontent.com/elyra-ai/elyra/v3.15.0/etc/generic/requirements-elyra.txt' && curl --fail -H 'Cache-Control: no-cache' -L https://raw.githubusercontent.com/elyra-ai/elyra/v3.15.0/etc/generic/requirements-elyra.txt --output requirements-elyra.txt && python3 -m pip install packaging && python3 -m pip freeze > requirements-current.txt && python3 bootstrapper.py --pipeline-name 'test-pipeline' --cos-endpoint http://localhost:9000 --cos-bucket customer-bucket --cos-directory 'test-pipeline-0526191149' --cos-dependencies-archive 'data_processing-data-processing-node.tar.gz' --file 'data_processing.py' "
+    ],
+    task_id="Data_Processing",
+    env_vars={
+        "ELYRA_RUNTIME_ENV": "airflow",
+        "AWS_ACCESS_KEY_ID": "minio",
+        "AWS_SECRET_ACCESS_KEY": "minio123",
+        "ELYRA_ENABLE_PIPELINE_INFO": "True",
+        "ELYRA_RUN_NAME": "test-pipeline-{{ ts_nodash }}",
+    },
+    resources={
+        "request_cpu": "1",
+        "request_memory": "2G",
+    },
+    volumes=[],
+    volume_mounts=[],
+    secrets=[],
+    annotations={},
+    labels={},
+    tolerations=[],
+    in_cluster=True,
+    config_file="None",
+    dag=dag,
+)
+
+
+# Operator source: data_analysis.py
+
+op_data_analysis_node = KubernetesPodOperator(
+    name="Data_Analysis",
+    namespace="airflow-elyra",
+    image="continuumio/anaconda3:2023.07-2",
+    cmds=["sh", "-c"],
+    arguments=[
+        "mkdir -p ./jupyter-work-dir/ && cd ./jupyter-work-dir/ && echo 'Downloading https://raw.githubusercontent.com/elyra-ai/elyra/v3.15.0/elyra/airflow/bootstrapper.py' && curl --fail -H 'Cache-Control: no-cache' -L https://raw.githubusercontent.com/elyra-ai/elyra/v3.15.0/elyra/airflow/bootstrapper.py --output bootstrapper.py && echo 'Downloading https://raw.githubusercontent.com/elyra-ai/elyra/v3.15.0/etc/generic/requirements-elyra.txt' && curl --fail -H 'Cache-Control: no-cache' -L https://raw.githubusercontent.com/elyra-ai/elyra/v3.15.0/etc/generic/requirements-elyra.txt --output requirements-elyra.txt && python3 -m pip install packaging && python3 -m pip freeze > requirements-current.txt && python3 bootstrapper.py --pipeline-name 'test-pipeline' --cos-endpoint http://localhost:9000 --cos-bucket customer-bucket --cos-directory 'test-pipeline-0526191149' --cos-dependencies-archive 'data_analysis-data-analysis-node.tar.gz' --file 'data_analysis.py' "
+    ],
+    task_id="Data_Analysis",
+    env_vars={
+        "ELYRA_RUNTIME_ENV": "airflow",
+        "AWS_ACCESS_KEY_ID": "minio",
+        "AWS_SECRET_ACCESS_KEY": "minio123",
+        "ELYRA_ENABLE_PIPELINE_INFO": "True",
+        "ELYRA_RUN_NAME": "test-pipeline-{{ ts_nodash }}",
+    },
+    resources={
+        "request_cpu": "1",
+        "request_memory": "2G",
+    },
+    volumes=[],
+    volume_mounts=[],
+    secrets=[],
+    annotations={},
+    labels={},
+    tolerations=[],
+    in_cluster=True,
+    config_file="None",
+    dag=dag,
+)
+
+op_data_analysis_node << op_data_processing_node
