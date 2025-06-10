8e075bf42f20d3aaf2e31add0d9c0cbcdf4c2aee
Aman Singh Chandel <amanchandel4@gmail.com>
2025-06-11 03:19:10 +0530

    Pushed DAG financial-0610214909

---
+++ financial-0610214909.py
@@ -0,0 +1,82 @@
+from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
+from kubernetes.client import models as k8s
+from airflow.providers.cncf.kubernetes.secret import Secret
+from airflow import DAG
+import pendulum
+
+args = {
+    "project_id": "financial-0610214909",
+}
+
+dag = DAG(
+    dag_id="financial-0610214909",
+    default_args=args,
+    schedule="@once",
+    start_date=pendulum.today("UTC").add(days=-1),
+    description="""
+Created with Elyra 4.0.0.dev0 pipeline editor using `financial.pipeline`.
+    """,
+    is_paused_upon_creation=False,
+)
+
+
+# Operator source: work/elyra-poc/pipelines/FINANCIAL_DATA/financial_data_processor.py
+
+op_2e17f0c2_14be_437d_8d3e_b040ec4b6fcc = KubernetesPodOperator(
+    name="financial_data_processor",
+    namespace="airflow-elyra",
+    image="continuumio/anaconda3:2024.02-1",
+    cmds=["sh", "-c"],
+    arguments=[
+        "mkdir -p ./jupyter-work-dir/ && cd ./jupyter-work-dir/ && echo 'Downloading https://raw.githubusercontent.com/elyra-ai/elyra/main/elyra/airflow/bootstrapper.py' && curl --fail -H 'Cache-Control: no-cache' -L https://raw.githubusercontent.com/elyra-ai/elyra/main/elyra/airflow/bootstrapper.py --output bootstrapper.py && echo 'Downloading https://raw.githubusercontent.com/elyra-ai/elyra/main/etc/generic/requirements-elyra.txt' && curl --fail -H 'Cache-Control: no-cache' -L https://raw.githubusercontent.com/elyra-ai/elyra/main/etc/generic/requirements-elyra.txt --output requirements-elyra.txt && python3 -m pip install packaging && python3 -m pip freeze > requirements-current.txt && python3 bootstrapper.py --pipeline-name 'financial' --cos-endpoint http://minio.minio-system.svc.cluster.local:9000 --cos-bucket elyra-airflow --cos-directory 'financial-0610214909' --cos-dependencies-archive 'financial_data_processor-2e17f0c2-14be-437d-8d3e-b040ec4b6fcc.tar.gz' --file 'work/elyra-poc/pipelines/FINANCIAL_DATA/financial_data_processor.py' "
+    ],
+    task_id="financial_data_processor",
+    env_vars={
+        "ELYRA_RUNTIME_ENV": "airflow",
+        "AWS_ACCESS_KEY_ID": "minio",
+        "AWS_SECRET_ACCESS_KEY": "minio123",
+        "ELYRA_ENABLE_PIPELINE_INFO": "True",
+        "ELYRA_RUN_NAME": "financial-{{ ts_nodash }}",
+    },
+    volumes=[],
+    volume_mounts=[],
+    secrets=[],
+    annotations={},
+    labels={},
+    tolerations=[],
+    in_cluster=True,
+    config_file=None,
+    dag=dag,
+)
+
+
+# Operator source: work/elyra-poc/pipelines/FINANCIAL_DATA/financial_data_visualizer.py
+
+op_1df94797_469c_4a32_b1c2_d8b51ee1c259 = KubernetesPodOperator(
+    name="financial_data_visualizer",
+    namespace="airflow-elyra",
+    image="continuumio/anaconda3:2024.02-1",
+    cmds=["sh", "-c"],
+    arguments=[
+        "mkdir -p ./jupyter-work-dir/ && cd ./jupyter-work-dir/ && echo 'Downloading https://raw.githubusercontent.com/elyra-ai/elyra/main/elyra/airflow/bootstrapper.py' && curl --fail -H 'Cache-Control: no-cache' -L https://raw.githubusercontent.com/elyra-ai/elyra/main/elyra/airflow/bootstrapper.py --output bootstrapper.py && echo 'Downloading https://raw.githubusercontent.com/elyra-ai/elyra/main/etc/generic/requirements-elyra.txt' && curl --fail -H 'Cache-Control: no-cache' -L https://raw.githubusercontent.com/elyra-ai/elyra/main/etc/generic/requirements-elyra.txt --output requirements-elyra.txt && python3 -m pip install packaging && python3 -m pip freeze > requirements-current.txt && python3 bootstrapper.py --pipeline-name 'financial' --cos-endpoint http://minio.minio-system.svc.cluster.local:9000 --cos-bucket elyra-airflow --cos-directory 'financial-0610214909' --cos-dependencies-archive 'financial_data_visualizer-1df94797-469c-4a32-b1c2-d8b51ee1c259.tar.gz' --file 'work/elyra-poc/pipelines/FINANCIAL_DATA/financial_data_visualizer.py' "
+    ],
+    task_id="financial_data_visualizer",
+    env_vars={
+        "ELYRA_RUNTIME_ENV": "airflow",
+        "AWS_ACCESS_KEY_ID": "minio",
+        "AWS_SECRET_ACCESS_KEY": "minio123",
+        "ELYRA_ENABLE_PIPELINE_INFO": "True",
+        "ELYRA_RUN_NAME": "financial-{{ ts_nodash }}",
+    },
+    volumes=[],
+    volume_mounts=[],
+    secrets=[],
+    annotations={},
+    labels={},
+    tolerations=[],
+    in_cluster=True,
+    config_file=None,
+    dag=dag,
+)
+
+op_1df94797_469c_4a32_b1c2_d8b51ee1c259 << op_2e17f0c2_14be_437d_8d3e_b040ec4b6fcc
